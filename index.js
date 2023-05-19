d90d9ad2ea6440f5c747b3ec7bce9a75e6494c91
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-19 13:24:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,20 @@ let allowedOrigins = [
   "http://localhost:1234",
 ];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("common"));
