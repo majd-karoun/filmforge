787031601e70d0af2bce98b31a37b15b57fb7158
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-19 13:18:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,25 +15,10 @@ const Genre = Models.Genre;
 
 let allowedOrigins = [
   "https://filmforge.herokuapp.com",
-  "https://filmforge.herokuapp.com/login",
   "http://localhost:1234",
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("common"));
